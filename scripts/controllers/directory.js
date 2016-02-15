0f4058fead157ebbe6e3d515748ed7afa527322e
Peter Toth <tothpeter08@gmail.com>
2016-02-15 22:21:02 +0100

    Adding a form event

--- scripts/controllers/directory.js
+++ scripts/controllers/directory.js
@@ -7,4 +7,16 @@ angular.module('directoryApp')
     {name: 'Kalina Tech', age: 12},
     {name: 'Ben', age: 11}
   ];
+
+  $scope.addPerson = function() {
+    var newPerson = {
+      name: $scope.name,
+      age: $scope.age
+    }
+
+    $scope.list.push(newPerson)
+
+    $scope.name = "";
+    $scope.age = "";
+  };
 });
